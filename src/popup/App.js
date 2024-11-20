8bdb89a70ee5151cfafac4b2f78802e2a4b77a38
Richa Bharti <richa.bharti1@gmail.com>
2024-11-21 01:37:37 +0530

    Mic app
    Signed-off-by: Richa Bharti <richa.bharti1@gmail.com>

--- src/popup/App.js
+++ src/popup/App.js
@@ -1,12 +1,64 @@
-import logo from 'url:../images/icon256.png';
+import {Box, Button} from '@mui/material';
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
+import {useState} from 'react';
 
 
 export const App = () => {
+    const [recording, setRecording] = useState(false);
+    const [playButtonDisabled,setPlayButtonDisabled] = useState(true);
+
+    let mediaRecorder;
+    let audioChunks = [];
+    let audioBlob;
+    let audioUrl;
+
+    const startRecording= async()=> {
+        try {
+            // Request access to the microphone
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.ondataavailable = (event) => {
+                audioChunks.push(event.data);
+            };
+
+            mediaRecorder.onstop = () => {
+                audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+                audioUrl = URL.createObjectURL(audioBlob);
+            };
+
+            mediaRecorder.start();
+
+        } catch (err) {
+            console.error('Error accessing microphone: ', err);
+            alert('Unable to access your microphone. Please make sure your device has a working microphone.');
+        }
+    }
+
+    function stopRecording() {
+        if (mediaRecorder && mediaRecorder.state === 'recording') {
+            mediaRecorder.stop();
+        }
+    }
+
+
+    const handRecording = () => {
+        if(recording === false){
+            startRecording();
+        }
+        else{
+            stopRecording();
+            setPlayButtonDisabled(false);
+        }
+        setRecording(!startRecording);
+    };
     return (
-        <>
-            <img src={logo} alt="Logo"/>
-            <h1>Popup Window</h1>
-            <div>v{chrome.runtime.getManifest().version}</div>
-        </>
+        <Box sx={{width: '150px'}}>
+            <Button sx={{marginBottom:'10px',width:'130px'}} variant="contained" onClick={() => handRecording()}>Recording
+                <div style={{background:'red',marginLeft:'10px', height:'25px'}}>{recording ? <StopCircleIcon/> : <PlayCircleFilledIcon/>}</div>
+            </Button>
+            <Button variant="contained" sx={{width:'130px',}} disabled={playButtonDisabled}>Play Button</Button>
+        </Box>
     );
 };
